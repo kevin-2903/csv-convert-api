e1889d554f221a8d45a8d2613aabeae5a46f2de0
kevin-2903 <146953136+kevin-2903@users.noreply.github.com>
2025-05-05 12:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if not firebase_admin._apps:
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL", "")
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40ai-finance-app-2def9.iam.gserviceaccount.com"
         }
         cred = credentials.Certificate(cred_dict)
     except Exception as e:
